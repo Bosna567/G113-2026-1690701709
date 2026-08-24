929087a71406a61b07489ea3dfd49fb7129ced45
Bosna567 <aditapsuksumran2@gmail.com>
2026-08-24 16:35:56 +0700

    Add lab02 header

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,4 +1,11 @@
-﻿namespace Lab02
+﻿/*
+ * Student ID :1290701709
+ * Name       :Aditap Suksamran
+ * Section    :129B
+ * No.        : N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lab02
 {
     internal class Program
     {
