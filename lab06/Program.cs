ecff5247f6a124c743bf597c9e95fc802e7b8339
Bosna567 <aditapsuksumran2@gmail.com>
2026-09-21 16:53:47 +0700

    lab06 again

--- lab06/Program.cs
+++ lab06/Program.cs
@@ -43,18 +43,35 @@
             //{
             //    Console.WriteLine("The door stay shut.");
             //}
-            int Hp = 100;
-            int HeroAttack = 20;
-            Console.WriteLine("GAME TITLE : Adventures of the End");
+            int WitchHp = 100;
+            int Attack = 30;
+            int SpecialAttack = 80;
+            int MonHp = 100;
+            int MonAttack = 80;
+            int MonSpecialAttack = 50;
+
+
+            Console.WriteLine("GAME TITLE : Vertex Adventure");
+            Console.WriteLine("Hero Vs Monster");
             Console.WriteLine("Action 1: Attack");
-            Console.WriteLine("Action 2: Run");
+            Console.WriteLine("Action 2: Special Attack");
 
             Console.WriteLine("Choose your action (1 or 2): ");
             bool isInputValid = int.TryParse(Console.ReadLine(), out int action);
 
-            if (isInputValid == false || action <1 || action >2)
+            if (isInputValid == false || action < 1 || action > 2)
             {
                 Console.WriteLine("Invalid Input, Please enter 1 or 2");
+            }
+            else if (action == 1)
+            {
+                MonHp -= Attack;
+                Console.WriteLine($"Witch attacked the monster! Monster's HP is now {MonHp}");
+            }
+            else if (action == 2)
+            {
+                MonHp -= SpecialAttack;
+                Console.WriteLine($"Witch used a special attack! Monster's HP is now {MonHp}");
             }
         }
     }
