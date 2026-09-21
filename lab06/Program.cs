9caf5ada077ae5f2b05dafcc362b5b931fac0abb
Bosna567 <aditapsuksumran2@gmail.com>
2026-09-21 21:58:15 +0700

    lab06athome

--- lab06/Program.cs
+++ lab06/Program.cs
@@ -43,36 +43,61 @@
             //{
             //    Console.WriteLine("The door stay shut.");
             //}
-            int WitchHp = 100;
-            int Attack = 30;
-            int SpecialAttack = 80;
+            int HeroHp = 100;
+            int Fireball = 30;
+            int CrimsonSolarFlare = 80;
+            int recovery = 40;
+            int mana = 100;
             int MonHp = 100;
             int MonAttack = 80;
             int MonSpecialAttack = 50;
+            int MonRecovery = 10;
+            int MonMana = 100;
 
+            Console.WriteLine("================================");
+            Console.WriteLine("||       Vertex Adventure     ||");
+            Console.WriteLine("||       Hero Vs Monster      ||");
+            Console.WriteLine("================================");
+            Console.WriteLine("Action 1: Fireball");
+            Console.WriteLine("Action 2: Crimson Solar Flare");
+            Console.WriteLine("Action 3: Recovery");
 
-            Console.WriteLine("GAME TITLE : Vertex Adventure");
-            Console.WriteLine("Hero Vs Monster");
-            Console.WriteLine("Action 1: Attack");
-            Console.WriteLine("Action 2: Special Attack");
-
-            Console.WriteLine("Choose your action (1 or 2): ");
+            Console.WriteLine("Choose your action (1 - 3): ");
             bool isInputValid = int.TryParse(Console.ReadLine(), out int action);
 
-            if (isInputValid == false || action < 1 || action > 2)
+            if (isInputValid == false || action < 1 || action > 3)
             {
-                Console.WriteLine("Invalid Input, Please enter 1 or 2");
+                Console.WriteLine("Invalid Input, Please enter 1-3");
             }
             else if (action == 1)
             {
-                MonHp -= Attack;
-                Console.WriteLine($"Witch attacked the monster! Monster's HP is now {MonHp}");
+                MonHp -= Fireball;
+                mana -= 20;
+                Console.WriteLine($"Hero used mana : 20");
+                Console.WriteLine($"Hero mana is now {mana}");
+                Console.WriteLine($"Hero used Fireball! Monster's HP is now {MonHp}");
             }
             else if (action == 2)
             {
-                MonHp -= SpecialAttack;
-                Console.WriteLine($"Witch used a special attack! Monster's HP is now {MonHp}");
+                MonHp -= CrimsonSolarFlare;
+                mana -= 50;
+                Console.WriteLine($"Hero used mana : 50");
+                Console.WriteLine($"Hero mana is now {mana}");
+                Console.WriteLine($"Hero used  Crimson Solar Flare! Monster's HP is now {MonHp}");
             }
+            else if (action == 3)
+            {
+                HeroHp += recovery;
+                mana -= 40;
+                Console.WriteLine($"Hero used mana : 40");
+                Console.WriteLine($"Hero mana is now {mana}");
+                Console.WriteLine("Hero used recovery!");
+                Console.WriteLine("Hero used Recover Energy!");
+                Console.WriteLine($"Hero's HP is now {HeroHp}");
+            }
+            Console.WriteLine("================================");
+            Console.WriteLine("           END TURN         ");
+            Console.WriteLine("================================");
         }
     }
 }
